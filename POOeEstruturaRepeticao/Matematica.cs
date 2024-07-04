46c4fcb83aff05c83b5b8df08fc7f07bab066b5c
Peterson Gregorio Araujo <petersonaraujo70@gmail.com>
2024-07-04 18:41:06 -0300

    Estrutura de Repetição

---
+++ POOeEstruturaRepeticao/Matematica.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOeEstruturaRepeticao
+{
+    class Matematica
+    {
+        public static int Exponenciacao(int Base, int Exp)
+        {
+            int Resultado = Base;
+            for (int i = 1; i < Exp; i++) 
+            {
+                Resultado = Resultado * Base;
+            }
+
+            return Resultado;
+        }
+    }
+}
---
+++ POOeEstruturaRepeticao/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace POOeEstruturaRepeticao
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int resultado = Matematica.Exponenciacao(2, 16);
+            
+            Console.WriteLine(resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
